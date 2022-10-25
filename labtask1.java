919746f5a4e11f7ca9be7cdbd96a2141edb673c0
JAYED <57245337+jayedbin12@users.noreply.github.com>
2022-10-25 20:51:07 +0600

    Add files via upload

---
+++ labtask1.java
@@ -0,0 +1,57 @@
+package com.hasib.java.practice;/*
+ * Created by S M Al Hasib on 10/14/22, 11:20 AM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 10/14/22, 11:20 AM
+ */
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Person {
+    protected final UUID id;
+    protected final String name;
+    protected final LocalDate dateOfBirth;
+
+    public Person(String name, int dayOfMonth, int month, int year) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.dateOfBirth = LocalDate.of(year, month, dayOfMonth);
+    }
+
+    protected String calculateAge() {
+        final Period diff = Period.between(dateOfBirth, LocalDate.now());
+        return diff.getYears() + " Years " +
+                diff.getMonths() + " Months " +
+                diff.getDays() + " Days ";
+    }
+
+    @Override
+    public String toString() {
+        return "name = " + name +
+                ", Age = " + this.calculateAge() + "\n";
+    }
+
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+
+        personList.add(new Student("Jayed", 23, 5, 1997));
+        personList.add(new Employee("Rafi", 7, 1, 1977));
+
+        personList.forEach(System.out::println);
+    }
+}
+
+class Student extends Person {
+    public Student(String name, int dayOfMonth, int month, int year) {
+        super(name, dayOfMonth, month, year);
+    }
+}
+
+class Employee extends Person {
+    public Employee(String name, int dayOfMonth, int month, int year) {
+        super(name, dayOfMonth, month, year);
+    }
+}
